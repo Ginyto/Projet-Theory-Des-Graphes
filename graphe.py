e3c8cf33e9de11655840c5a3dff21442be8547a2
Ginyto <agianlucca@gmail.com>
2021-12-07 17:59:16 +0100

    debut de l'initialisation de l'objet graphes

--- graphe.py
+++ graphe.py
@@ -2,6 +2,18 @@ from jarvis import *
 
 test = Jarvis("test")
 
+class Graphe:
+    
+    def __init__(self, name):
+        self.name = name
+        self.sommet = []
+    
+    def fill_sommet(self, etat):
+        self.sommet.append(etat)
+    
+    
+        
+
 
 
 
