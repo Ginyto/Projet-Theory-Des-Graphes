f2a8083757945ac7ba944e131751df2334509b33
Ginyto <agianlucca@gmail.com>
2021-12-07 19:32:13 +0100

    debut de la classe Graphe

--- graphe.py
+++ graphe.py
@@ -1,17 +1,14 @@
 from jarvis import *
 
-test = Jarvis("test")
 
 class Graphe:
+    
+    jarvis = Jarvis()
     
     def __init__(self, name):
         self.name = name
         self.sommet = [] #Tableau d'objet sommet
-        
-    def fill(self,tab,cosa):
-        self.tab.append(cosa)
-    
-        
+
 
 
 
