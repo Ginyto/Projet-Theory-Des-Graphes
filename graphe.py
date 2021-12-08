fdcbfd8a0b006dacffb1b1db6eb059b8e413f2d4
Ginyto <agianlucca@gmail.com>
2021-12-08 17:10:38 +0100

    Début de la fonction sommatisation -> generer des sommet dans un tableau d'objet

--- graphe.py
+++ graphe.py
@@ -1,4 +1,5 @@
 from jarvis import *
+from sommet import *
 
 class Graphe:
     
@@ -7,12 +8,18 @@ class Graphe:
     def __init__(self, fichier):
         self.name = fichier
         self.jarvis.initialisation(fichier)
+        Graphe.sommatisation(self)
+        
+    def sommatisation(self):
+        for i in range(0, self.jarvis.sommet):
+            print(i)
     
     
 
 automate = Graphe("test")
 
-automate.jarvis.affichage(automate.jarvis.mat)
+
+#automate.jarvis.affichage(automate.jarvis.mat)
 
 
 
