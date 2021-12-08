017ee6bef43dfd1c41eb18380903da53eeae3814
Ginyto <agianlucca@gmail.com>
2021-12-08 17:33:24 +0100

    Debut de la fonction arctisation qui va permettre de nourrire les sommet avec leurs chemin respectif et leurs poids

--- graphe.py
+++ graphe.py
@@ -10,13 +10,17 @@ class Graphe:
         self.registre = []
         self.jarvis.initialisation(fichier)
         Graphe.sommatisation(self)
+        Graphe.arctisation(self)
         
     def sommatisation(self):
         for i in range(0, self.jarvis.sommet):
             y = Sommet(i) #création des sommet
             self.registre.append(y)
 
-        print(self.registre)
+        #print(self.registre)
+    
+    def arctisation(self):
+        print("Hello there!")
     
     
     
