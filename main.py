eace9ed15df88147d14ac3e7204585c9dc6c4934
Ginyto <agianlucca@gmail.com>
2022-01-03 17:47:29 +0100

    creation d'une fonction de verification de saisie dans jarvis

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+from graphe import *
+
 menu = True
 
 while menu == True:
-    x = int(input("Quel graphe voulez-vous choisir ? (1 à 13) \n"))
-    while x<1 or x>13:
-        x = int(input("Erreur dans la saisie du graphe.\n Quel graphe voulez-vous choisir ? (1 à 13) \n"))
-    print("Lecture du grpahe x et stockage en mémoire de x\n")
+    choix_graphe = int(input("Quel graphe voulez-vous choisir ? (1 à 13) \n"))
+    
+    while choix_graphe < 1 and choix_graphe > 13:
+        choix_graphe = int(input("Erreur dans la saisie du graphe.\n Quel graphe voulez-vous choisir ? (1 à 13) \n"))
+    
+    automate = Graphe(str(choix_graphe))
+        
+    print("Lecture du graphe x et stockage en mémoire de x\n")
     print("Affichage de x\n")
     print("Floyd-Warshall de x\n")
     print("Existance de circuit absorbant\n") #(variable  booléan circuit par exemple)
+    
     if circuit == True:
         g = int(input("Voulez vous voir un autre graphe ? (1 : Oui. 2 : Non.)\n"))
         if g !=1:
